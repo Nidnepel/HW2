b710c25ef7bd9d7319db53f7eedb52fdfbd2957e
DmitriyL <dimalependin@gmail.com>
2022-12-31 07:47:53 +0300

    ConsoleReader rewritten as wrapper for static function and added exception checks, interface DataReader.java deleted.

--- src/ConsoleReader.java
+++ src/ConsoleReader.java
@@ -1,21 +1,42 @@
 import java.io.File;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
-public class ConsoleReader extends DataReader { // TODO: Возможно вынести в пакет
-
-    ConsoleReader() {
-        startDir = read();
+public class ConsoleReader {
+    public static File readRootDirectory() {
+        var in = new Scanner(System.in);
+        while (true) {
+            String input = safeRead(in);
+            var rootDirectory = new File(input);
+            if (checkAccessAndCorrectInputDirectory(rootDirectory)) {
+                return rootDirectory;
+            }
+        }
     }
 
-    private File read() {
-        var in = new Scanner(System.in);
-        String input = in.nextLine();
-        var dir = new File(input);
-        while (!dir.isDirectory()) {
-            System.out.println("This string is not correct path!\n Please input a correct path");
-            input = in.nextLine();
-            dir = new File(input);
+    private static String safeRead(Scanner in) {
+        String input = "";
+        while (input.equals("")) {
+            try {
+                input = in.nextLine();
+            } catch (NoSuchElementException ex) {
+                System.out.println("Please input root directory path!");
+            }
         }
-        return dir;
+        input = input.replace('/', File.pathSeparatorChar);
+        return input;
+    }
+
+    private static boolean checkAccessAndCorrectInputDirectory(File directory) {
+        boolean isCorrect = false;
+        try {
+            isCorrect = directory.isDirectory();
+            if (!isCorrect) {
+                System.out.println("This string is not correct path!\n Please input a correct path");
+            }
+        } catch (SecurityException ex) {
+            System.out.println("Entered directory is not available!");
+        }
+        return isCorrect;
     }
 }
--- src/DataReader.java
+++
@@ -1,9 +0,0 @@
-import java.io.File;
-
-public abstract class DataReader {
-    File startDir; //TODO: как сделать приватным?
-
-    public File getStartDir() {
-        return startDir;
-    }
-}
