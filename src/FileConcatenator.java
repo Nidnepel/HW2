ce8a197699679d63010ba3267ed069213c918ec2
DmitriyL <dimalependin@gmail.com>
2023-01-08 20:45:17 +0300

    Added a javadoc

--- src/FileConcatenator.java
+++ src/FileConcatenator.java
@@ -1,5 +1,8 @@
 import java.util.List;
 
+/**
+ * Static class for concantenate texts from files
+ */
 public class FileConcatenator {
     public static String concatenateFiles(List<Vertex> vertexList) {
         StringBuilder result = new StringBuilder();
