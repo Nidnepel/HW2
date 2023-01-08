5b66d32e4cfbced4d20d1f06f3135261513be4ff
DmitriyL <dimalependin@gmail.com>
2023-01-08 19:44:54 +0300

    Added a javadoc

--- src/ConsoleReader.java
+++ src/ConsoleReader.java
@@ -2,7 +2,15 @@ import java.io.File;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+/**
+ * A static class responsible for securely reading the root directory path from the console.
+ */
 public class ConsoleReader {
+    /**
+     * Reads the path to the root directory from the console and returns an object of type file
+     *
+     * @return File type variable built on the root directory.
+     */
     public static File readRootDirectory() {
         var in = new Scanner(System.in);
         while (true) {
@@ -14,6 +22,12 @@ public class ConsoleReader {
         }
     }
 
+    /**
+     * Safe read data
+     *
+     * @param in - Scanner for read data from console
+     * @return input string
+     */
     private static String safeRead(Scanner in) {
         String input = "";
         while (input.equals("")) {
@@ -27,6 +41,12 @@ public class ConsoleReader {
         return input;
     }
 
+    /**
+     * Check access and correct input directory.
+     *
+     * @param directory - intended directory
+     * @return Is entered directory correct and available
+     */
     private static boolean checkAccessAndCorrectInputDirectory(File directory) {
         boolean isCorrect = false;
         try {
