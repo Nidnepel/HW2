d375f2ac8ea90a95d085ea59dad2c2d5be9169b3
DmitriyL <dimalependin@gmail.com>
2023-01-08 20:05:05 +0300

    Added a javadoc

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -4,7 +4,14 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Class to get a list of file dependencies.
+ */
 public class TextAnalyzer {
+    /**
+     * @param data - text from processing file
+     * @return List of names dependent files
+     */
     public List<String> getRequiredFileNames(String data) {
         var requiredFileNames = new ArrayList<String>();
         Pattern pattern = Pattern.compile("require '.*'");
