17afcc965ecf36468f9726c7fc5ba573a146ee16
DmitriyL <dimalependin@gmail.com>
2023-01-08 20:00:03 +0300

    Added a javadoc

--- src/VertexBuilder.java
+++ src/VertexBuilder.java
@@ -1,15 +1,32 @@
 import java.io.File;
 import java.util.List;
 
+/**
+ * Data processing class for Vertex.
+ */
 class VertexBuilder {
+    /**
+     * TextReader variable for read text from file.
+     */
     private final TextReader reader;
+    /**
+     * TextAnalyzer variable for get requires from text.
+     */
     private final TextAnalyzer analyzer;
 
+    /**
+     * Constructor
+     */
     public VertexBuilder() {
         reader = new TextReader();
         analyzer = new TextAnalyzer();
     }
 
+    /**
+     * @param file          - Vertex file
+     * @param rootDirectory - root directory
+     * @return Vertex variable based on the received information.
+     */
     public Vertex build(File file, File rootDirectory) {
         var rootPathLen = rootDirectory.getPath().length();
         String name = file.getPath().substring(rootPathLen + 1);
